25b10abd98533f3164077e88cf72ec6f8358d584
avrobullet <douglasa@unbc.ca>
2021-08-11 10:56:17 -0700

    Object pointing via parent=globalenv()

---
+++ ObjectReferencing.R
@@ -0,0 +1,49 @@
+### Check object referencing ---------------------------------------------------
+
+# Create variables
+mat_a = matrix(nrow=100,ncol=100)
+mat_b = newPointer(mat_a)
+all_matrices = list(mat_a,
+                    mat_b)
+
+
+# Create object pointer function
+newPointer=function(inputValue){ 
+  object=new.env(parent=globalenv()) 
+  object$value=inputValue 
+  class(object)='pointer'
+  
+  return(object) 
+} 
+
+# Change contents of a matrix
+alterMatrix <- function(m, k){
+  for(i in 1:100) {
+    for(j in 1:100){
+      m[i,j] <- k
+    }
+  }
+  return(m)
+}
+
+# Check matrices prior to being processed
+# print(mat_a)
+# print(mat_b$value)
+
+# Process matrices
+mat_a <- alterMatrix(mat_a,1)
+mat_b$value <- alterMatrix(mat_b$value,2)
+
+# Check matrices prior to being processed
+print(mat_a)
+print(mat_b$value)
+
+#'VERIFIED: mat_b needs to be altered as its own entity (it lives in another
+#'          environment). In addition, all_matrices seems to no update itself
+#'          with mat_a after it was processed. Using pointers would make the 
+#'          world of difference to my R coding!
+
+# Check object size
+print(object.size(mat_a))
+print(object.size(mat_b))
+print(object.size(all_matrices))
---
+++ main.R
@@ -0,0 +1,8 @@
+library(Seurat)
+library(Giotto)
+library(hash)
+
+# Using Giotto test single cell data
+# data(mini_giotto_single_cell)
+# print(mini_giotto_single_cell@raw_exprs)
+source("ObjectReferencing.R")
