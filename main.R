eaf26a0c6c7eed9339d47df73da8285dfa348ca5
avrobullet <douglasa@unbc.ca>
2021-08-12 09:44:17 -0700

    Updates

--- main.R
+++ main.R
@@ -1,26 +1,36 @@
 library(Seurat)
 library(dplyr)
 library(patchwork)
-library(hash)
 library(matrixStats)
 
 # Object Referencing/Pointing (hacking)
 #source(paste0(getwd(),"/ObjectReferencing.R"))
-# Seurat Demo Pipeline
-# iterations <- 1
-# seuratdemo_metrics <- matrix(nrow=iterations, ncol=1)
-# for(i in 1:iterations) {
-#   # Run metrics and pipeline
-#   start_time <- Sys.time()
-#   source(paste0(getwd(),"/SeuratIntro.R"))
-#   end_time <- Sys.time()
-#   # Save metrics
-#   seuratdemo_metrics[i,] <- end_time - start_time
-# }
-# print(seuratdemo_metrics)
-# avg_score <- sum(seuratdemo_metrics)
-# # avg_score = colMean(seuratdemo_metrics)
-# # print(sapply(seuratdemo_metrics, sd))
-# hist(seuratdemo_metrics)
-x <- matrix(rep(1:9), 3, 3)
-print(colSums(x)/3)
+my_stats <- function(mat,iterations){
+  mat_sum <- colSums(mat)
+  mat_avg <- mat_sum/iterations
+  mean_diff <- 0
+  print(mat)
+  print(mat_avg)
+  for(j in 1:iterations){
+    # Sum and squared
+    mean_diff <- mean_diff + (mat[j]-mat_avg)^2
+  }
+  # Divide by total per col size and square root
+  mean_diff <- sqrt(mean_diff/iterations)
+  print(mean_diff)
+}
+
+#Seurat Demo Pipeline
+iterations <- 10
+seuratdemo_metrics <- matrix(nrow=iterations, ncol=1)
+for(i in 1:iterations) {
+  # Run metrics and pipeline
+  start_time <- Sys.time()
+  source(paste0(getwd(),"/SeuratIntro.R"))
+  end_time <- Sys.time()
+  # Save metrics
+  seuratdemo_metrics[i,] <- end_time - start_time
+}
+print(seuratdemo_metrics)
+my_stats(seuratdemo_metrics, iterations)
+
