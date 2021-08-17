b23e4177fdbe1919dad341c19b8ae5aa88d799d6
Alex Douglas <alex@procogia.com>
2021-08-16 22:37:37 -0700

    Update main.R

--- main.R
+++ main.R
@@ -8,4 +8,28 @@ library(sjmisc)
 # Object Referencing/Pointing (hacking)
 #source(paste0(getwd(),"/ObjectReferencing.R"))
 #source(paste0(getwd(),"/SeuratIntro.R"))
-source(paste0(getwd(),"/AWSAccess.R"))
+#source(paste0(getwd(),"/AWSAccess.R"))
+# demo <- c()
+# demo_runs <- c('Run1',
+#                'Run2',
+#                'Run2',
+#                'Run3')
+# checkDuplicates <- function(run){
+#   if(length(demo) == 0){
+#     demo <<- c(demo, run)
+#   }
+#   else {
+#     duplicate_found = FALSE
+#     for(i in seq_len(length(demo))){
+#       if(run == demo[i]){
+#         duplicate_found = TRUE
+#         break
+#       }
+#     }
+#     if(!duplicate_found){demo <<- c(demo,run)}
+#   }
+# }
+# sapply(demo_runs, checkDuplicates)
+# print(demo)
+words <- as.list(strsplit('My_name_is_BOB','_')[[1]])
+print(words[[length(words)]])
