d78847d0e5935f14f884af30262ac7b32dfa5e56
Alexander Douglas <alexanderdouglas@Alexanders-MacBook-Pro-2.local>
2021-08-17 22:41:29 -0700

    Updating

--- AWSAccess.R
+++ AWSAccess.R
@@ -28,46 +28,72 @@ config_loading <- list(
 #   Panel <- c()
 # )
 runs_found <- c()
+# Identify all unique runs
+checkDuplicates <- function(run){
+  if(length(runs_found) == 0){
+    runs_found <<- c(runs_found, run)
+  }
+  else {
+    duplicate_found = FALSE
+    for(i in seq_len(length(runs_found))){
+      if(run == runs_found[i]){
+        duplicate_found = TRUE
+        break
+      }
+    }
+    if(!duplicate_found){runs_found <<- c(runs_found,run)}
+  }
+}
 
 nanostring_bucket <- aws.s3::get_bucket("nanostring-external-smi-commercial")
-nanostring_bucket_table <- data.table::rbindlist(nanostring_bucket)
+# tempfile <- tempfile()
+# save_object(object = 's3://nanostring-external-smi-commercial/100-plex/Run90070_100pIO-1nM-D3_20210317_155221_S1/CellStatsDir/Run90070_20210317_155221_Cell_Stats_F001.csv',
+#             file = tempfile)
+# write.csv(tempfile)
+s3read_using(FUN = read.csv, object = "s3://nanostring-external-smi-commercial/100-plex/Run90070_100pIO-1nM-D3_20210317_155221_S1/CellStatsDir/Run90070_20210317_155221_Cell_Stats_F001.csv")
 # For all keys...
 for(key in nanostring_bucket_table[,Key]){
   # ...only focus on keys matching the user's designated results path...
   if(str_contains(key, config_main$resultspath)){
-    #print(key)
-    # ...to load the necessary FOV annotations
+    # ...to load the necessary FOV annotations.
     if(str_contains(key, config_loading$annotfile_location)){
       message(paste0('Annotation File Found: ',key))
-      config_loading$annotfile <- aws.s3::s3read_using(FUN = read.csv, 
-                                                       bucket = nanostring_bucket, 
+      config_loading$annotfile <- aws.s3::s3read_using(FUN = read.csv,
+                                                       bucket = nanostring_bucket,
                                                        object = key)
     }
-    # ...to load the necessary data files
+    # ...to load the necessary data files.
     if(str_contains(key, 'Run')){
+      # Break down Run# keys
       run_parameters <- as.list(strsplit(key,'/')[[1]])
-      # Identify all unique runs
-      checkDuplicates <- function(run){
-        if(length(runs_found) == 0){
-          runs_found <<- c(runs_found, run)
-        }
-        else {
-          duplicate_found = FALSE
-          for(i in seq_len(length(runs_found))){
-            if(run == runs_found[i]){
-              duplicate_found = TRUE
-              break
-            }
-          }
-          if(!duplicate_found){runs_found <<- c(runs_found,run)}
+      # print(key)
+      # Identify Run# experiment and create corresponding directories if they don't exist
+      # Create main experiment folder (Ex: Run####)
+      if(!file.exists(run_parameters[[3]])){
+        dir.create(run_parameters[[3]])
+      }
+      # # Cell stats directory (CellStatsDir)
+      if(!file.exists(paste0(run_parameters[[3]],'/CellStatsDir'))){
+        dir.create(paste0(run_parameters[[3]],'/CellStatsDir'))
+      } else { # Download data if directory exists
+
+      }
+      # Cell Gene expression directory (Run####_Iter#_PCIter#_TSR_v#)
+      if(grepl('Run[0-9]+_Iter[0-9]+_PCIter[0]+_TSR[0-9]+.[0-9]+_DF[0-9]+_v[0-9]',run_parameters[6])){
+        if(!file.exists(paste0(run_parameters[[3]],'/',run_parameters[6]))){
+          dir.create(paste0(run_parameters[[3]],'/',run_parameters[6]))
+        } else { # Download data if directory exists
+
         }
       }
-      sapply(run_parameters[6], checkDuplicates)
+      # Download corresponding folder
+      # if(str_contains(key,'Run90089_Iter1_PCIter0_TSR0.5_DF1_v5')){
+      #   save_object(object = paste0("s3://nanostring-external-smi-commercial/",key), file = 'filtered_gene_bc_matrices')
+      # }
     }
     #'gem <- runloadingmodule(config_loading,
     #'                        loading_results_filepath = "intermediate_results/loading_results.RData",
     #'                        loading_input_hash_filepath = "intermediate_results/hashes/loading_hash.RData"))
   }
 }
-print(runs_found)
 
--- main.R
+++ main.R
@@ -4,11 +4,18 @@ library(patchwork)
 library(matrixStats)
 library(aws.s3)
 library(sjmisc)
+library(parallel)
+library(Giotto)
 
 # Object Referencing/Pointing (hacking)
 #source(paste0(getwd(),"/ObjectReferencing.R"))
 #source(paste0(getwd(),"/SeuratIntro.R"))
-#source(paste0(getwd(),"/AWSAccess.R"))
+source(paste0(getwd(),"/AWSAccess.R"))
+# multi_demo <- list(
+#   demo1 <- c(),
+#   demo2 <- c(),
+#   demo3 <- c()
+# )
 # demo <- c()
 # demo_runs <- c('Run1',
 #                'Run2',
@@ -29,7 +36,9 @@ library(sjmisc)
 #     if(!duplicate_found){demo <<- c(demo,run)}
 #   }
 # }
+# 
+# mclapply(demo_item in multi_demo) {
+#   
+# }
 # sapply(demo_runs, checkDuplicates)
 # print(demo)
-words <- as.list(strsplit('My_name_is_BOB','_')[[1]])
-print(words[[length(words)]])
